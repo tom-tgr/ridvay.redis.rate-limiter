3d5683f576c5e2348b1e5484292d9e7d44d764c0
Toms Gruzins <tomsgruzins@Gmail.com>
2024-12-09 11:43:41 +0200

    Exclude benchmarks when running tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.ts'],
+  testMatch: ['**/tests/**/*.test.ts','!**/benchmark.test.ts'],
   moduleFileExtensions: ['ts', 'js'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
